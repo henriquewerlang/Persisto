5d0cc21e7a2e4957b3fa1e26e5aa71b8b47f3945
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2020-01-15 22:10:34 -0300

    Setting up the things.

---
+++ Delphi.ORM.pas
@@ -0,0 +1,7 @@
+unit Delphi.ORM;
+
+interface
+
+implementation
+
+end.
---
+++ Tests/Delphi.ORM.Test.pas
@@ -0,0 +1,15 @@
+unit Delphi.ORM.Test;
+
+interface
+
+uses DUnitX.TestFramework;
+
+//type
+//  [TestFixture]
+//  TDelphiORMTest = class
+//  public
+//  end;
+
+implementation
+
+end.
