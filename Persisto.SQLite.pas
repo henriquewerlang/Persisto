7d0a62a2dda3889acd9adf54480553d3e48ea6e4
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2024-01-15 15:53:58 -0300

    Delphi 12 strings.

--- Persisto.SQLite.pas
+++ Persisto.SQLite.pas
@@ -70,74 +70,84 @@ end;
 function TDatabaseManipulatorSQLite.GetSchemaTablesScripts: TArray<String>;
 const
   PRIMARY_KEY_CONSTRAINT_SQL =
-    'select cast(''PK_'' || T.name as varchar(250)) Id,' +
-           'cast(''PK_'' || T.name as varchar(250)) Name,' +
-           'cast(PK.name as varchar(250)) FieldName ' +
-      'from PersistoDatabaseTable T,' +
-           'pragma_table_info(T.name) PK ' +
-     'where PK.pk = 1';
+    '''
+    select cast('PK_' || T.name as varchar(250)) Id,
+           cast('PK_' || T.name as varchar(250)) Name,
+           cast(PK.name as varchar(250)) FieldName
+      from PersistoDatabaseTable T,
+           pragma_table_info(T.name) PK
+     where PK.pk = 1
+    ''';
 
   DEFAULT_CONSTRAINT_SQL =
     'select null Id, null Name, null Value';
 
   FOREING_KEY_SQL =
-    'select cast(FK.id || ''.'' || FK."table" as varchar(250)) Id,' +
-           'cast(''FK_'' || T.name || ''_'' || FK."from" as varchar(250)) Name,' +
-           'cast(T.name as varchar(250)) IdTable,' +
-           'cast(FK."table" as varchar(250)) IdReferenceTable,' +
-           'cast(null as varchar(250)) ReferenceField ' +
-      'from PersistoDatabaseTable T,' +
-           'pragma_foreign_key_list(T.name) FK';
+    '''
+    select cast(FK.id || '.' || FK."table" as varchar(250)) Id,
+           cast('FK_' || T.name || '_' || FK."from" as varchar(250)) Name,
+           cast(T.name as varchar(250)) IdTable,
+           cast(FK."table" as varchar(250)) IdReferenceTable,
+           cast(null as varchar(250)) ReferenceField
+      from PersistoDatabaseTable T,
+           pragma_foreign_key_list(T.name) FK
+    ''';
 
   SEQUENCES_SQL =
-    'select cast(name as varchar(250)) Id,' +
-           'cast(name as varchar(250)) Name ' +
-      'from sqlite_sequence';
+    '''
+    select cast(name as varchar(250)) Id,
+           cast(name as varchar(250)) Name
+      from sqlite_sequence
+    ''';
 
   TABLE_SQL =
-    'select cast(T.name as varchar(250)) Id,' +
-           'cast((select cast(''PK_'' || T.name as varchar(250)) ' +
-                   'from pragma_table_info(T.name) PK ' +
-                  'where PK.pk = 1) as varchar(250)) IdPrimaryKeyConstraint,' +
-           'cast(T.name as varchar(250)) Name ' +
-      'from sqlite_master T ' +
-     'where T.type = ''table'' ' +
-       'and not T.name like ''sqlite_%''';
+    '''
+    select cast(T.name as varchar(250)) Id,
+           cast((select cast('PK_' || T.name as varchar(250))
+                   from pragma_table_info(T.name) PK
+                  where PK.pk = 1) as varchar(250)) IdPrimaryKeyConstraint,
+           cast(T.name as varchar(250)) Name
+      from sqlite_master T
+     where T.type = 'table'
+       and not T.name like 'sqlite_%'
+    ''';
 
   COLUMNS_SQL =
-    'select cast(T.name || ''#'' || C.name as varchar(250)) Id,' +
-           'null IdDefaultConstraint,' +
-           'cast(T.name as varchar(250)) IdTable,' +
-           'case lower(substr(type, 1, iif(instr(type, ''('') > 0, 7, length(type)))) ' +
-              'when ''varchar'' then 5 ' +
-              'when ''integer'' then 1 ' +
-              'when ''char'' then 2 ' +
-              'when ''smallint'' then 3 ' +
-              'when ''numeric'' then 4 ' +
-              'when ''bigint'' then 16 ' +
-              'else 0 ' +
-           'end FieldType,' +
-           'cast(C.name as varchar(250)) Name,' +
-           '"notnull" Required,' +
-           'cast(substr(type, instr(type, '','') + 1, length(type) - instr(type, '','') - 1) as integer) Scale,' +
-           'cast(substr(type, instr(type, ''('') + 1, coalesce(nullif(instr(type, '',''), 0), length(type)) - instr(type, ''('') - 1) as integer) Size,' +
-           'case lower(type)' +
-              // Date
-              'when ''date'' then 1 ' +
-              // DateTime
-              'when ''datetime'' then 2 ' +
-              // Time
-              'when ''time'' then 3 ' +
-              // Text
-              'when ''text'' then 4 ' +
-              // Unique Identifier
-              'when ''uniqueidentifier'' then 5 ' +
-              // Boolean
-              'when ''boolean'' then 6 ' +
-              'else 0 ' +
-           'end SpecialType ' +
-      'from PersistoDatabaseTable T,' +
-           'pragma_table_info(T.name) C';
+    '''
+    select cast(T.name || '#' || C.name as varchar(250)) Id,
+           null IdDefaultConstraint,
+           cast(T.name as varchar(250)) IdTable,
+           case lower(substr(type, 1, iif(instr(type, '(') > 0, 7, length(type))))
+              when 'varchar' then 5
+              when 'integer' then 1
+              when 'char' then 2
+              when 'smallint' then 3
+              when 'numeric' then 4
+              when 'bigint' then 16
+              else 0
+           end FieldType,
+           cast(C.name as varchar(250)) Name,
+           "notnull" Required,
+           cast(substr(type, instr(type, ',') + 1, length(type) - instr(type, ',') - 1) as integer) Scale,
+           cast(substr(type, instr(type, '(') + 1, coalesce(nullif(instr(type, ','), 0), length(type)) - instr(type, '(') - 1) as integer) Size,
+           case lower(type)
+              -- Date
+              when 'date' then 1
+              -- DateTime
+              when 'datetime' then 2
+              -- Time
+              when 'time' then 3
+              -- Text
+              when 'text' then 4
+              -- Unique Identifier
+              when 'uniqueidentifierchar' then 5
+              -- Boolean
+              when 'boolean' then 6
+              else 0
+           end SpecialType
+      from PersistoDatabaseTable T,
+           pragma_table_info(T.name) C
+    ''';
 
   function CreateView(const Name, SQL: String): String;
   begin
