fb5b7f1ff2f6813b389242cede1c7c2316a8052d
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2024-06-11 19:30:30 -0300

    Removed the FastMM5 dependency.

--- Tests/Persisto.Tests.dpr
+++ Tests/Persisto.Tests.dpr
@@ -3,11 +3,9 @@
 {$STRONGLINKTYPES ON}
 
 uses
-  FastMM5 in '..\Externals\FastMM5\FastMM5.pas',
   System.SysUtils,
   TestInsight.DUnitX,
   DUnitX.TestFramework,
-  DUnitX.MemoryLeakMonitor.FastMM5,
   Persisto.DataSet.Test in 'Persisto.DataSet.Test.pas',
   Persisto.Classes.Loader.Test in 'Persisto.Classes.Loader.Test.pas',
   Persisto.Mapper.Test in 'Persisto.Mapper.Test.pas',
@@ -20,15 +18,7 @@ uses
   Persisto.Test.Connection in 'Persisto.Test.Connection.pas';
 
 begin
-  FastMM_EnterDebugMode;
-
-  FastMM_OutputDebugStringEvents := [];
-  FastMM_LogToFileEvents := [mmetUnexpectedMemoryLeakSummary, mmetUnexpectedMemoryLeakDetail];
-  FastMM_MessageBoxEvents := [mmetDebugBlockDoubleFree, mmetVirtualMethodCallOnFreedObject];
-
-  FastMM_DeleteEventLogFile;
-
-  FastMM_BeginEraseFreedBlockContent;
+  ReportMemoryLeaksOnShutdown := True;
 
   TestInsight.DUnitX.RunRegisteredTests;
 end.
