249e14773e3a332a3487587992e9c5ea82aa899e
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2024-09-14 11:50:40 -0300

    Fix the schema SQL from SQL Server, marking wrong types and sizes.

--- Persisto.SQLServer.pas
+++ Persisto.SQLServer.pas
@@ -56,7 +56,7 @@ begin
     tkFloat:
       Result := 'numeric';
     tkChar, tkWChar:
-      Result := 'char';
+      Result := 'nchar';
     tkInt64:
       Result := 'bigint';
     tkString, tkLString, tkWString, tkUString:
@@ -69,101 +69,118 @@ end;
 function TDatabaseManipulatorSQLServer.GetSchemaTablesScripts: TArray<String>;
 const
   DEFAULT_CONSTRAINT_SQL =
-    'select cast(parent_object_id as varchar(20)) + ''.'' + cast(parent_column_id as varchar(20)) Id,' +
-           'name,' +
-           'substring(definition, 2, len(definition) - 2) Value ' +
-      'from sys.default_constraints';
+    '''
+    select cast(parent_object_id as varchar(20)) + '.' + cast(parent_column_id as varchar(20)) Id,
+           name,
+           substring(definition, 2, len(definition) - 2) Value
+      from sys.default_constraints
+    ''';
 
   FOREING_KEY_SQL =
-    'select cast(object_id as varchar(20)) Id,' +
-           'name,' +
-           'cast(parent_object_id as varchar(20)) IdTable,' +
-           'cast(referenced_object_id as varchar(20)) IdReferenceTable,' +
-           'null ReferenceField ' +
-      'from sys.foreign_keys FK';
+    '''
+    select cast(object_id as varchar(20)) Id,
+           name,
+           cast(parent_object_id as varchar(20)) IdTable,
+           cast(referenced_object_id as varchar(20)) IdReferenceTable,
+           null ReferenceField
+      from sys.foreign_keys
+    ''';
 
   FOREING_KEY_COLUMS_SQL =
-    'select cast(FKC.constraint_object_id as varchar(20)) + ''.'' + cast(constraint_column_id as varchar(20)) Id,' +
-           'cast(FKC.constraint_object_id as varchar(20)) IdForeignKey,' +
-           'RC.name ' +
-      'from sys.foreign_keys FK ' +
-      'join sys.foreign_key_columns FKC ' +
-        'on FKC.constraint_object_id = FK.object_id ' +
-      'join sys.columns RC ' +
-        'on RC.object_id = FKC.referenced_object_id ' +
-       'and RC.column_id = FKC.referenced_column_id';
+    '''
+    select cast(FKC.constraint_object_id as varchar(20)) + '.' + cast(constraint_column_id as varchar(20)) Id,
+           cast(FKC.constraint_object_id as varchar(20)) IdForeignKey,
+           RC.name
+      from sys.foreign_keys FK
+      join sys.foreign_key_columns FKC
+        on FKC.constraint_object_id = FK.object_id
+      join sys.columns RC
+        on RC.object_id = FKC.referenced_object_id
+       and RC.column_id = FKC.referenced_column_id
+    ''';
 
   PRIMARY_KEY_CONSTRAINT_SQL =
-    'select cast(PK.object_id as varchar(20)) Id,' +
-           'PK.name,' +
-           'C.name FieldName ' +
-      'from sys.key_constraints PK ' +
-      'join sys.index_columns IC ' +
-        'on IC.object_id = PK.parent_object_id ' +
-       'and IC.index_id = PK.unique_index_id ' +
-      'join sys.columns C ' +
-        'on C.object_id = IC.object_id ' +
-       'and C.column_id = IC.column_id ' +
-     'where PK.type = ''PK''';
+    '''
+    select cast(PK.object_id as varchar(20)) Id,
+           PK.name,
+           C.name FieldName
+      from sys.key_constraints PK
+      join sys.index_columns IC
+        on IC.object_id = PK.parent_object_id
+       and IC.index_id = PK.unique_index_id
+      join sys.columns C
+        on C.object_id = IC.object_id
+       and C.column_id = IC.column_id
+     where PK.type = 'PK'
+    ''';
 
   SEQUENCES_SQL =
-    'select cast(object_id as varchar(20)) Id,' +
-            'name ' +
-      'from sys.sequences';
+    '''
+    select cast(object_id as varchar(20)) Id,
+            name
+      from sys.sequences
+    ''';
 
   TABLE_SQL =
-       'select cast(T.object_id as varchar(20)) Id,' +
-              'cast(PK.object_id as varchar(20)) IdPrimaryKeyConstraint,' +
-              'T.name ' +
-         'from sys.tables T ' +
-    'left join sys.key_constraints PK ' +
-           'on PK.parent_object_id = T.object_id ' +
-          'and PK.type = ''PK''';
+    '''
+       select cast(T.object_id as varchar(20)) Id,
+              cast(PK.object_id as varchar(20)) IdPrimaryKeyConstraint,
+              T.name
+         from sys.tables T
+    left join sys.key_constraints PK
+           on PK.parent_object_id = T.object_id
+          and PK.type = 'PK'
+    ''';
 
   COLUMNS_SQL =
-    'select cast(C.object_id as varchar(20)) + ''.'' + cast(C.column_id as varchar(20)) Id,' +
-           '(select cast(DF.parent_object_id as varchar(20)) + ''.'' + cast(DF.parent_column_id as varchar(20)) ' +
-              'from sys.default_constraints DF ' +
-             'where DF.parent_object_id = C.object_id ' +
-               'and DF.parent_column_id = C.column_id) IdDefaultConstraint,' +
-           'cast(T.object_id as varchar(20)) IdTable,' +
-           'case system_type_id ' +
-              // String
-              'when 167 then 5 ' +
-              // Integer
-              'when 56 then 1 ' +
-              // Char
-              'when 175 then 2 ' +
-              // Enumeration
-              'when 48 then 3 ' +
-              // Float
-              'when 108 then 4 ' +
-              // Int64
-              'when 127 then 16 ' +
-              'else 0 ' +
-           'end FieldType,'+
-           'C.name,' +
-           'iif(C.is_nullable = 0, 1, 0) Required,'+
-           'C.scale Scale,' +
-           'C.max_length Size,' +
-           'case system_type_id ' +
-              // Date
-              'when 40 then 1 ' +
-              // DateTime
-              'when 61 then 2 ' +
-              // Time
-              'when 41 then 3 ' +
-              // Text
-              'when 167 then iif(max_length = -1, 4, 0)  ' +
-              // Unique Identifier
-              'when 36 then 5 ' +
-              // Boolean
-              'when 104 then 6 ' +
-              'else 0 ' +
-           'end SpecialType '+
-      'from sys.columns C ' +
-      'join sys.tables T ' +
-        'on T.object_id = C.object_id';
+    '''
+    select cast(C.object_id as varchar(20)) + '.' + cast(C.column_id as varchar(20)) Id,
+           (select cast(DF.parent_object_id as varchar(20)) + '.' + cast(DF.parent_column_id as varchar(20))
+              from sys.default_constraints DF
+             where DF.parent_object_id = C.object_id
+               and DF.parent_column_id = C.column_id) IdDefaultConstraint,
+           cast(T.object_id as varchar(20)) IdTable,
+           case system_type_id
+              -- String
+              when 231 then 5
+              -- Integer
+              when 56 then 1
+              -- Char
+              when 239 then 2
+              -- Enumeration
+              when 48 then 3
+              -- Float
+              when 108 then 4
+              -- Int64
+              when 127 then 16
+              else 0
+           end FieldType,
+           C.name,
+           iif(C.is_nullable = 0, 1, 0) Required,
+           C.scale Scale,
+           case
+              when system_type_id in (231, 239) then C.max_length / 2
+              else C.max_length
+           end Size,
+           case system_type_id
+              -- Date
+              when 40 then 1
+              -- DateTime
+              when 61 then 2
+              -- Time
+              when 41 then 3
+              -- Text
+              when 231 then iif(max_length = -1, 4, 0)
+              -- Unique Identifier
+              when 36 then 5
+              -- Boolean
+              when 104 then 6
+              else 0
+           end SpecialType
+      from sys.columns C
+      join sys.tables T
+        on T.object_id = C.object_id
+    ''';
 
   function CreateView(const Name, SQL: String): String;
   begin
