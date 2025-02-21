dccace4014d74c0499f498f6815509a4fc4bf7e8
Henrique Gottardi Werlang <henriquewerlang@hotmail.com>
2025-02-21 08:16:24 -0300

    Creating temporary view in Postgres.

--- Persisto.PostgreSQL.pas
+++ Persisto.PostgreSQL.pas
@@ -247,7 +247,7 @@ const
 
   function CreateView(const Name, SQL: String): String;
   begin
-    Result := Format('create or replace view PersistoDatabase%s as (%s)', [Name, SQL]);
+    Result := Format('create temp view PersistoDatabase%s as (%s)', [Name, SQL]);
   end;
 
 begin
